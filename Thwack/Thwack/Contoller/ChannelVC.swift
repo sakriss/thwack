c574c455701cd0a849bb5b568121520c5a3fefe2
Scott Kriss <skriss@deloitte.com>
2019-07-17 13:07:07 -0700

    Added the slide menu

---
+++ Thwack/Thwack/Contoller/ChannelVC.swift
@@ -0,0 +1,19 @@
+//
+//  ChannelVC.swift
+//  Thwack
+//
+//  Created by Kriss, Scott (US - Seattle) on 7/17/19.
+//  Copyright © 2019 Scott Kriss. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.revealViewController()?.rearViewRevealWidth = self.view.frame.size.width - 60
+    }
+
+}
