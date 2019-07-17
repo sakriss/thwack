d0f224ff0fdf86fcdd18f632dbcfeb5f1a28f17d
Scott Kriss <skriss@deloitte.com>
2019-07-17 13:50:29 -0700

    Added tableview and colors to the controller vc

---
+++ Thwack/Thwack/View/GradientView.swift
@@ -0,0 +1,37 @@
+//
+//  GradientView.swift
+//  Thwack
+//
+//  Created by Kriss, Scott (US - Seattle) on 7/17/19.
+//  Copyright © 2019 Scott Kriss. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+
+    @IBInspectable var topColor: UIColor = UIColor.blue {
+        didSet {
+            self.setNeedsLayout()
+            
+        }
+    }
+    
+    @IBInspectable var bottomColor: UIColor = UIColor.green {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+
+}
