d78414bbc302c18d5d5159aa298d1d3192de5579
Scott Kriss <skriss@deloitte.com>
2019-07-17 12:23:48 -0700

    created initial app

---
+++ Thwack/Thwack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Thwack
+//
+//  Created by Kriss, Scott (US - Seattle) on 7/17/19.
+//  Copyright © 2019 Scott Kriss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
