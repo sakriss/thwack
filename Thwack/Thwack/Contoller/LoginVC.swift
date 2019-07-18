a1122ad8b2147d22aa269711e3590848f590391d
Scott Kriss <skriss@deloitte.com>
2019-07-18 15:35:23 -0700

    Added create and log in services

--- Thwack/Thwack/Contoller/LoginVC.swift
+++ Thwack/Thwack/Contoller/LoginVC.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class LoginVC: UIViewController {
 
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    
+    @IBAction func loginBtnPressed(_ sender: Any) {
+        guard let username = usernameTxt.text , usernameTxt.text != "" else {return}
+        guard let pass = passwordTxt.text , passwordTxt.text != "" else {return}
+        
+        AuthService.instance.registerUser(email: username, password: pass) { (success) in
+            if success {
+                print("user signed in!!!!!")
+            }
+        }
+        
+    }
+    
     @IBAction func createAccountBtnPressed(_ sender: Any) {
         performSegue(withIdentifier: TO_CREATE_ACCOUNT, sender: nil)
     }
--- Thwack/Thwack/Utilities/Constants.swift
+++ Thwack/Thwack/Utilities/Constants.swift
@@ -10,8 +10,9 @@ import Foundation
 
 typealias CompletionHandler = (_ Success: Bool) -> ()
 
-let BASE_URL = "https://thwack-chat.herokuapp.com/v1"
+let BASE_URL = "https://thwack-chat.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
+let URL_LOGIN = "\(BASE_URL)account/login"
 
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
@@ -21,3 +22,6 @@ let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
 
+let HEADER = [
+    "Content-Type": "application/json; charset=utf-8"
+]
