3cadc74dcb0bdac4376e34efd3bb332bc299b80d
Scott Kriss <skriss@deloitte.com>
2019-07-24 15:04:51 -0700

    Added color around avatar on channel screen

--- Thwack/Thwack/Contoller/ChannelVC.swift
+++ Thwack/Thwack/Contoller/ChannelVC.swift
@@ -15,6 +15,7 @@ class ChannelVC: UIViewController {
         
     }
     
+    @IBOutlet weak var userImage: CircleImage!
     @IBAction func loginBtnPressed(_ sender: Any) {
         performSegue(withIdentifier: TO_LOGIN, sender: nil)
     }
@@ -22,6 +23,19 @@ class ChannelVC: UIViewController {
         super.viewDidLoad()
 
         self.revealViewController()?.rearViewRevealWidth = self.view.frame.size.width - 60
+        NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+    }
+    
+    @objc func userDataDidChange(_ notif: Notification) {
+        if AuthService.instance.isLoggedIn {
+            loginBtn.setTitle(UserDataService.instance.name, for: .normal)
+            userImage.image = UIImage(named: UserDataService.instance.avatarName)
+            userImage.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+        } else {
+            loginBtn.setTitle("Login", for: .normal)
+            userImage.image = UIImage(named: "menuProfileIcon")
+            userImage.backgroundColor = UIColor.clear
+        }
     }
 
 }
